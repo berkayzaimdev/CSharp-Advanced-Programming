d316e37ce8b03eda769258da643a15fff4a9837a
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-12 11:50:47 +0300

    Added Exception Handling

---
+++ src/05_Exception_Handling/Program.cs
@@ -0,0 +1,33 @@
+﻿try
+{
+    while(true)
+    {
+        var key = Console.ReadKey();
+        if(key.Key is ConsoleKey.A)
+        {
+            throw new CustomException("test");
+        }
+        else
+        {
+
+        }
+    }
+    int a = 1, b = 0;
+    int c = a / b;
+}
+
+catch (Exception ex)
+{
+
+}
+
+class CustomException : Exception
+{
+    public CustomException() : base("I'm a custom exception")
+    {
+    }
+
+    public CustomException(string message) : base(message)
+    {
+    }
+}
