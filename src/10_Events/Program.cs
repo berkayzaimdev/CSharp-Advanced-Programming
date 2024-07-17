f1d96bddbeae9c58acc3eff44808811c3266e6e0
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-17 17:28:21 +0300

    Added Events

---
+++ src/10_Events/Program.cs
@@ -0,0 +1,36 @@
+﻿MyEventPublisher m = new();
+// x.XHandler = ...; public keyword olsa dahi, bir sınıfın instance'ından o delegate'i DEĞİŞTİREMEYİZ.
+MyEventPublisher.XHandler xHandler = new MyEventPublisher.XHandler(() => Console.Write("test"));
+m.MyEvent += () => Console.WriteLine("Event tetiklendi..");
+
+class MyEventPublisher
+{
+    public delegate void XHandler();
+
+    XHandler xDelegate; // field, add ve remove kullanılırsa gerek olmaz
+
+    public event XHandler MyEvent // property
+    // add ve remove blokları isteğe bağlı tanımlanabilir
+    // add => += sonucu ne olacak?
+    // remove => -= sonucu ne olacak?
+    {
+        add // property-field mantığı
+        {
+            Console.WriteLine("Event'e metot bağlandı.");
+            xDelegate += value;
+        }
+
+        remove
+        {
+            Console.WriteLine("Event'ten metot koparıldı.");
+            xDelegate -= value;
+        }
+    }
+
+    public void RaiseEvent()
+    {
+        // MyEvent.Invoke(); // add ve remove kullanılırsa hata verir
+
+        xDelegate.Invoke(); // add ve remove kullanılırsa gerek olmaz
+    }
+}
