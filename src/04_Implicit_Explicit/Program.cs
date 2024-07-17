b86bf3b50773dc6b7faaf234ce5a4addff9f82f0
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-17 15:16:50 +0300

    Added Covariance and Contravariance

--- src/04_Implicit_Explicit/Program.cs
+++ src/04_Implicit_Explicit/Program.cs
@@ -23,7 +23,7 @@ A a2 = (A)new B();
 B b = (B)new A();
 
 class A
-{
+{    // sen sağ tarafta A'dan bir instance oluşturuyorsun ve bu oluşturduğun instance'ı, B tipindeki bir değişkene atamak istiyorsun. Bu normal koşulda gerçekleşmez explicit ile (B) gibi belirtmemiz gerekir.
     public static explicit operator B(A i) // A type'ına sahip olan i; (B) kullanımı ile B type'ına dönüşecek
     {
         return new B { };
@@ -32,6 +32,7 @@ class A
 
 class B
 {
+    // sen sağ tarafta B'den bir instance oluşturuyorsun ve bu oluşturduğun instance'ı, A tipindeki bir değişkene atamak istiyorsun. Implicit ile bu dönüşüm otomatik olarak gerçekleşecektir.
     public static implicit operator A(B i) // B type'ına sahip olan i; A type'ına dönüşecek
     {
         return new A { };
---
+++ src/08_Covariance_Contravariance/Program.cs
@@ -0,0 +1,98 @@
+﻿#region Covariance
+// Her veri türü object'ten türer; object in türevidir. Bu sebeple bu sağ-sol dengesizliği solun lehine olduğu zaman, Covariance kuralı devreye girer.
+// Covariance ve Contravariance durumlarında derleyici bir problem çıkarmaz bize.
+
+object[] isimler = new string[5] { "Ahmet", "Mehmet", "Ali", "Mustafa", "Hilmi" };
+
+#region Array Types
+
+object[] x = new string[5];
+x[0] = "wadsd";
+x[1] = 123; // covariance durumlarında tehlike arz eder. runtime'da hata fırlatacaktır. string dizisine int verilemez çünkü
+
+IEnumerable<object> arabalar = new List<string> { "Tesla", "Volvo", "TOGG" };
+
+IEnumerable<A> asd = new List<B> { new(), new() };
+
+#endregion
+
+#region Delegete Types
+
+Func<A> funcA = GetB; // covariance için uygun bir delegate
+B GetB() => new(); // B türünden değer döndüren fonksiyon
+
+#endregion
+
+#region Return Types
+
+//class Animal
+//{
+//    public virtual Animal X() => new();
+//}
+//class Cat : Animal
+//{
+//    public override Cat X() => new(); // override keyword ile üst sınıftaki metot ile eşleşti
+//}
+
+#endregion
+
+#region Generic Types
+
+//IAnimal<object> objectAnimals = new Animal<string>();
+//IAnimal<A> aAnimals = new Animal<B>();
+
+//interface IAnimal<out T> { } // out keyword burada covariance davranışın gerçekleşmesini sağlar. kullanmadığımız durumda compiler error verir
+//// out sadece interface ve delegate türlerde kullanılabilir
+//class Animal<T> : IAnimal<T> { }
+
+#endregion
+
+
+#endregion
+
+#region Contravariance
+// sağ-sol dengesizliği sağın lehine ise bu durum Contravariance'a işaret eder
+
+#region Delegete Types
+
+Action<string> xDelegate = XMethod;
+void XMethod(object o) { }
+
+Action<B> bDelegate = AMethod;
+void AMethod(A a) { }
+
+#endregion
+
+#region Generic Types
+
+IAnimal<string> stringAnimals = new Animal<object>();
+IAnimal<B> aAnimals = new Animal<A>();
+
+interface IAnimal<in T> { } // in keyword burada contravariance davranışın gerçekleşmesini sağlar. kullanmadığımız durumda compiler error verir
+// B ile B'yi karşılıyoruz, A'yı da karşılıyoruz
+class Animal<T> : IAnimal<T> { }
+
+#endregion
+
+#endregion
+
+#region Özet
+
+/*
+    * Covariance ve Contravariance terimleri; array, delegate, return ve generic typelar için implicit referans dönüşümlerini ifade eder. Polymorphism' e benzer bir yapıdır.
+    
+    * out keyword output davranışlarda kullanılırken, in keyword ise input davranışlarda kullanılır.
+    
+    * delegate Covariancer<in T>(T t);
+    * delegate T Contravariancer<out T>();
+*/
+
+#endregion
+
+class A 
+{
+    public virtual A X() => new();
+}
+class B : A {
+    public override B X() => new(); // override keyword ile üst sınıftaki metot ile eşleşti
+}
