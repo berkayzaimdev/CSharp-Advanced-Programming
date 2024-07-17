424dd372ce10925e316dc2c3b9dc0a2913fa9d20
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-17 17:44:05 +0300

    Added Built In Delegates

---
+++ src/11_Built_In_Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Action
+
+// Action<T1,T2,...,Tn> ---> dönüş değeri yok
+Action action1 = () => Console.WriteLine("I'm an action!");
+Action<string> action2 = (s) => Console.WriteLine("I'm an generic action 1!");
+Action<bool, string> action3 = (b, s) => Console.WriteLine("I'm an generic action 2!");
+
+#endregion
+
+#region Func
+
+// Func<T1,T2,...,Tn> ---> parametreler (T1,T2,...Tn-1), dönüş değeri Tn
+Func<string> func1 = () => "I'm a function!";
+Func<bool, string, bool> func2 = (b, s) => true;
+
+#endregion
+
+#region Predicate
+
+Predicate<int> predicate1 = i => i > 0; // pozitif mi
+Predicate<string> predicate2 = s => s.Contains(" "); // space var mı
+
+Console.WriteLine("Result without space: "+ predicate2("asdasd"));
+Console.WriteLine("Result with space: "+ predicate2("   "));
+
+#endregion
