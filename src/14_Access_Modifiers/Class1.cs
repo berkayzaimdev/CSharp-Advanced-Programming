5910057878ab61861fdf224bb6d7ebcf72422cc1
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-18 15:38:55 +0300

    Added Access Modifiers

---
+++ src/14_Access_Modifiers/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace _14_Access_Modifiers
+{
+    public class Class1
+    {
+        public void X()
+        {
+
+        }
+
+        protected void Y()
+        { 
+        }
+
+        void Z() // default olarak private
+        {
+            Y();
+        }
+
+        internal void Q() // aynı proje haricinde erişilemez
+        {
+            
+        }
+
+        private protected void W()
+        {
+
+        }
+    }
+
+    class Class2 : Class1
+    {
+        public Class2()
+        {
+            Class1 c = new();
+            // c.Y(); // protected, instance üzerinden erişimi engellediği için kullanılamaz!
+            base.Y(); // class üzerinden erişimi ise mümkün kılar. base'den gelir (keyword'e gerek yok)
+
+            W(); // class üzerinden erişim, protected olduğu için mümkündür.
+        }
+    }
+}
